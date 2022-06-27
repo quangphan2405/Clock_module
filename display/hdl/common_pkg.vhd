777bed5370fcb6d419709d092e73cd22b4ead80e
Quang Phan <quang.phan@tum.de>
2022-06-27 02:59:23 +0200

    Add header and correct syntax error

--- display/hdl/common_pkg.vhd
+++ display/hdl/common_pkg.vhd
@@ -1,3 +1,12 @@
+--------------------------------------------------------------------------------
+-- Author       : Quang Phan
+-- Author email : quang.phan@tum.de
+-- Create Date  : 27/06/2022
+-- Project Name : Project Lab IC Design
+-- Module Name  : common_pkg.vhd
+-- Description  : Common package that will be used globally in display module
+--------------------------------------------------------------------------------
+
 library ieee;
 
 package common_pkg is
