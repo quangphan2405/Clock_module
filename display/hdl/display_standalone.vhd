944cd73af2ac603ccde03dd64ddf6aeee019cca7
Quang Phan <quang.phan@tum.de>
2022-07-22 04:09:22 +0200

    Create starting and act signals for different modules

--- display/hdl/display_standalone.vhd
+++ display/hdl/display_standalone.vhd
@@ -81,10 +81,16 @@ architecture behavior of display_standalone is
 	signal led_countdown_ring : std_logic := '0';
 	signal led_switch_act : std_logic := '0';
 	signal led_switch_on : std_logic := '0';
-	
+
 	-- Act signals
-	signal lcd_stopwatch_act : std_logic;
-	
+    signal lcd_time_act      : std_logic;
+    signal lcd_alarm_act     : std_logic;
+    signal lcd_alarm_snooze  : std_logic;
+    signal lcd_switchon_act  : std_logic;
+    signal lcd_switchoff_act : std_logic;
+    signal lcd_countdown_act : std_logic;
+    signal lcd_stopwatch_act : std_logic;
+
 	-- STW signals
 	signal cs : std_logic_vector(6 downto 0);
 	signal ss : std_logic_vector(6 downto 0);
@@ -97,7 +103,6 @@ architecture behavior of display_standalone is
 	signal key_mode_imp : std_logic;
 	signal key_plus_minus : std_logic;
 	signal key_enable : std_logic;
-	
 
     -- Reset signals
 	signal reset : std_logic := '1';		-- Internal reset signal, high for at least 16 cycles
@@ -173,7 +178,7 @@ begin
         en_10               => en_10,
         -- Time
         fsm_time_start      => fsm_time_start,
-        lcd_time_act        => '1',
+        lcd_time_act        => lcd_time_act,
         lcd_time_data       => time_data,
         -- Date
         fsm_date_start      => fsm_date_start,
@@ -181,21 +186,21 @@ begin
         lcd_date_data       => DD_MM_YY_c,
         -- Alarm
         fsm_alarm_start     => fsm_alarm_start,
-        lcd_alarm_act       => '0',
-        lcd_alarm_snooze    => '1',
+        lcd_alarm_act       => lcd_alarm_act,
+        lcd_alarm_snooze    => lcd_alarm_snooze,
         lcd_alarm_data      => HH_MM_c,
         -- Switch ON
         fsm_switchon_start  => fsm_switchon_start,
-        lcd_switchon_act    => '1',
-        lcd_switchon_data   => HH_MM_SS_c,
+        lcd_switchon_act    => lcd_switchon_act,
+        lcd_switchon_data   => time_data,
         -- Switch ON
         fsm_switchoff_start => fsm_switchoff_start,
-        lcd_switchoff_act   => '0',
-        lcd_switchoff_data  => HH_MM_SS_c,
+        lcd_switchoff_act   => lcd_switchoff_act,
+        lcd_switchoff_data  => time_data,
         -- Countdown
         fsm_countdown_start => fsm_countdown_start,
-        lcd_countdown_act   => '0',
-        lcd_countdown_data  => HH_MM_SS_c,
+        lcd_countdown_act   => lcd_countdown_act,
+        lcd_countdown_data  => time_data,
         -- Stopwatch
         fsm_stopwatch_start => fsm_stopwatch_start,
         lcd_stopwatch_act   => lcd_stopwatch_act,
@@ -205,128 +210,145 @@ begin
         lcd_rw              => LCD_RW,
         lcd_rs              => LCD_RS,
         lcd_data            => LCD_DATA
-        -- Output debug
-        -- led_alarm_act       => led_alarm_act,
-        -- led_alarm_ring      => led_alarm_ring,
-        -- led_countdown_act   => led_countdown_act,
-        -- led_countdown_ring  => led_countdown_ring,
-        -- led_switch_act      => led_switch_act,
-        -- led_switch_on       => led_switch_on
-    );
-    
-    stw_i : entity work.stopwatch
-    port map (
-        clk => clk,
-        en_100 => en_100,
-        reset => reset,
-        fsm_stopwatch_start => fsm_stopwatch_start,
-        key_plus_imp => key_plus_imp,
-        key_minus_imp => key_minus_imp,
-        key_action_imp => key_action_imp,
-        lcd_stopwatch_act => lcd_stopwatch_act,
-        cs => cs,
-        ss => ss,
-        mm => mm,
-        hh => hh
-    );
-    
-    key_ctrl_i : entity work.key_control
-    port map (
-        clk                 => clk,
-        reset               => reset,
-        en_100              => en_100,
-        en_10               => en_10,
-        btn_action          => BTNC,
-        btn_minus          => BTNL,
-        btn_plus          => BTNR,
-        btn_mode          => BTNU,
-        action_imp => key_action_imp,
-        action_long => key_action_long,
-        plus_imp => key_plus_imp,
-        minus_imp => key_minus_imp,
-        mode_imp => key_mode_imp,
-        plus_minus => key_plus_minus,
-        enable => key_enable
     );
 
-    -- Generate start
-    GEN_START : process(clk)
+    stw_i : entity work.stopwatch
+    port map (
+        clk                 => clk,
+        en_100              => en_100,
+        reset               => reset,
+        fsm_stopwatch_start => fsm_stopwatch_start,
+        key_plus_imp        => key_plus_imp,
+        key_minus_imp       => key_minus_imp,
+        key_action_imp      => key_action_imp,
+        lcd_stopwatch_act   => lcd_stopwatch_act,
+        cs                  => cs,
+        ss                  => ss,
+        mm                  => mm,
+        hh                  => hh
+    );
+
+    key_ctrl_i : entity work.key_control
+    port map (
+        clk         => clk,
+        reset       => reset,
+        en_100      => en_100,
+        en_10       => en_10,
+        btn_action  => BTNC,
+        btn_minus   => BTNL,
+        btn_plus    => BTNR,
+        btn_mode    => BTNU,
+        action_imp  => key_action_imp,
+        action_long => key_action_long,
+        plus_imp    => key_plus_imp,
+        minus_imp   => key_minus_imp,
+        mode_imp    => key_mode_imp,
+        plus_minus  => key_plus_minus,
+        enable      => key_enable
+    );
+
+    -- Generate start & act
+    GEN_START_ACT : process(clk)
     begin
         if (clk'EVENT and clk = '1') then
             if (reset = '1') then
-                cnt_r <= 0;
+                cnt_r               <= 0;
                 fsm_time_start      <= '0';
                 fsm_date_start      <= '0';
                 fsm_alarm_start     <= '0';
                 fsm_switchon_start  <= '0';
                 fsm_switchoff_start <= '0';
                 fsm_countdown_start <= '0';
-                fsm_stopwatch_start <= '1';
+                fsm_stopwatch_start <= '0';
             else
---                if ( cnt_r = 2000 ) then
---                    fsm_time_start      <= '1';
---                    fsm_date_start      <= '0';
---                    fsm_alarm_start     <= '0';
---                    fsm_switchon_start  <= '0';
---                    fsm_switchoff_start <= '0';
---                    fsm_countdown_start <= '0';
---                    fsm_stopwatch_start <= '0';
---                elsif ( cnt_r = 12000 ) then
---                    fsm_time_start      <= '0';
---                    fsm_date_start      <= '1';
---                    fsm_alarm_start     <= '0';
---                    fsm_switchon_start  <= '0';
---                    fsm_switchoff_start <= '0';
---                    fsm_countdown_start <= '0';
---                    fsm_stopwatch_start <= '0';
---                elsif ( cnt_r = 22000 ) then
---                    fsm_time_start      <= '0';
---                    fsm_date_start      <= '0';
---                    fsm_alarm_start     <= '1';
---                    fsm_switchon_start  <= '0';
---                    fsm_switchoff_start <= '0';
---                    fsm_countdown_start <= '0';
---                    fsm_stopwatch_start <= '0';
---                elsif ( cnt_r = 32000 ) then
---                    fsm_time_start      <= '0';
---                    fsm_date_start      <= '0';
---                    fsm_alarm_start     <= '0';
---                    fsm_switchon_start  <= '1';
---                    fsm_switchoff_start <= '0';
---                    fsm_countdown_start <= '0';
---                    fsm_stopwatch_start <= '0';
---                elsif ( cnt_r = 42000 ) then
---                    fsm_time_start      <= '0';
---                    fsm_date_start      <= '0';
---                    fsm_alarm_start     <= '0';
---                    fsm_switchon_start  <= '0';
---                    fsm_switchoff_start <= '1';
---                    fsm_countdown_start <= '0';
---                    fsm_stopwatch_start <= '0';
---                elsif ( cnt_r = 52000 ) then
---                    fsm_time_start      <= '0';
---                    fsm_date_start      <= '0';
---                    fsm_alarm_start     <= '0';
---                    fsm_switchon_start  <= '0';
---                    fsm_switchoff_start <= '0';
---                    fsm_countdown_start <= '1';
---                    fsm_stopwatch_start <= '0';
---                elsif ( cnt_r = 62000 ) then
---                    fsm_time_start      <= '0';
---                    fsm_date_start      <= '0';
---                    fsm_alarm_start     <= '0';
---                    fsm_switchon_start  <= '0';
---                    fsm_switchoff_start <= '0';
---                    fsm_countdown_start <= '0';
---                    fsm_stopwatch_start <= '1';
---                end if;
+                if ( cnt_r = 20000 ) then
+                    fsm_time_start      <= '1';
+                    fsm_date_start      <= '0';
+                    fsm_alarm_start     <= '0';
+                    fsm_switchon_start  <= '0';
+                    fsm_switchoff_start <= '0';
+                    fsm_countdown_start <= '0';
+                    fsm_stopwatch_start <= '0';
+                elsif ( cnt_r = 70000 ) then
+                    lcd_time_act        <= '1';
+                elsif ( cnt_r = 120000 ) then
+                    lcd_time_act        <= '0';
+                    fsm_time_start      <= '0';
+                    fsm_date_start      <= '1';
+                    fsm_alarm_start     <= '0';
+                    fsm_switchon_start  <= '0';
+                    fsm_switchoff_start <= '0';
+                    fsm_countdown_start <= '0';
+                    fsm_stopwatch_start <= '0';
+                elsif ( cnt_r = 220000 ) then
+                    lcd_alarm_snooze    <= '0';
+                    fsm_time_start      <= '0';
+                    fsm_date_start      <= '0';
+                    fsm_alarm_start     <= '1';
+                    fsm_switchon_start  <= '0';
+                    fsm_switchoff_start <= '0';
+                    fsm_countdown_start <= '0';
+                    fsm_stopwatch_start <= '0';
+                elsif ( cnt_r = 260000 ) then
+                    lcd_alarm_act       <= '1';
+                elsif ( cnt_r = 295000 ) then
+                    lcd_alarm_act       <= '0';
+                    lcd_alarm_snooze    <= '1';
+                elsif ( cnt_r = 320000 ) then
+                    lcd_alarm_snooze    <= '0';
+                    lcd_switchon_act    <= '0';
+                    fsm_time_start      <= '0';
+                    fsm_date_start      <= '0';
+                    fsm_alarm_start     <= '0';
+                    fsm_switchon_start  <= '1';
+                    fsm_switchoff_start <= '0';
+                    fsm_countdown_start <= '0';
+                    fsm_stopwatch_start <= '0';
+                elsif ( cnt_r = 370000 ) then
+                    lcd_switchon_act    <= '1';
+                elsif ( cnt_r = 420000 ) then
+                    lcd_switchon_act    <= '0';
+                    fsm_time_start      <= '0';
+                    fsm_date_start      <= '0';
+                    fsm_alarm_start     <= '0';
+                    fsm_switchon_start  <= '0';
+                    fsm_switchoff_start <= '1';
+                    fsm_countdown_start <= '0';
+                    fsm_stopwatch_start <= '0';
+                elsif ( cnt_r = 470000 ) then
+                    lcd_switchoff_act   <= '1';
+                elsif ( cnt_r = 520000 ) then
+                    lcd_switchoff_act   <= '0';
+                    fsm_time_start      <= '0';
+                    fsm_date_start      <= '0';
+                    fsm_alarm_start     <= '0';
+                    fsm_switchon_start  <= '0';
+                    fsm_switchoff_start <= '0';
+                    fsm_countdown_start <= '1';
+                    fsm_stopwatch_start <= '0';
+                elsif ( cnt_r = 570000 ) then
+                    lcd_countdown_act   <= '1';
+                elsif ( cnt_r = 620000 ) then
+                    lcd_countdown_act   <= '0';
+                    fsm_time_start      <= '0';
+                    fsm_date_start      <= '0';
+                    fsm_alarm_start     <= '0';
+                    fsm_switchon_start  <= '0';
+                    fsm_switchoff_start <= '0';
+                    fsm_countdown_start <= '0';
+                    fsm_stopwatch_start <= '1';
+                elsif ( cnt_r = 670000 ) then
+                    lcd_stopwatch_act   <= '1';
+                elsif ( cnt_r = 720000 ) then
+                    lcd_stopwatch_act   <= '0';
+                end if;
 
                 cnt_r <= cnt_r + 1;
-            
+
             end if;
         end if;
-    end process GEN_START;
-    
+    end process GEN_START_ACT;
+
     -- Generate the stopwatch data
     GEN_STW_DATA : process(clk)
     begin
@@ -340,11 +362,10 @@ begin
             end if;
         end if;
     end process GEN_STW_DATA;
-    
+
     -- Data assignment
     -- stw_data <= std_logic_vector(to_unsigned(stw_cnt_r, 28));
     stw_data <= hh & mm & ss & cs;
     time_data <= std_logic_vector(to_unsigned(data_cnt_r, 21));
-    
-    
+
 end architecture behavior;
